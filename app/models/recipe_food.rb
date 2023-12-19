fac879c3e94741b0f64e82b9a0902604ada62ed6
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-19 18:41:11 +0530

    Fix linter error

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,6 +1,6 @@
 class RecipeFood < ApplicationRecord
-    belongs_to :recipe
-    belongs_to :food
+  belongs_to :recipe
+  belongs_to :food
 
-    validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
