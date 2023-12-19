5fb819a64721dc3496fb08449de061f0c4c72a35
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-12-19 17:57:31 +0500

    Create validations on recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
 
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
+  validates :public, presence: true
 end
