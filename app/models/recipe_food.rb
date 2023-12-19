d2d1fce3f67b1ee903a261b863bf88c25cea8248
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-19 19:41:35 +0530

    Update recipe food model and migration file

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,6 +1,6 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :recipe, foreign_key: :cooking_time
-  belongs_to :food, foreign_key: :measurement_unit
+  belongs_to :recipe
+  belongs_to :food
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- db/migrate/20231219125248_create_recipe_foods.rb
+++ db/migrate/20231219125248_create_recipe_foods.rb
@@ -2,8 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :recipe_foods do |t|
       t.integer :quantity
-      t.references :recipe, foreign_key: { to_table: :cooking_times }
-      t.references :food, foreign_key: { to_table: :measurement_units }
+      t.references :recipe, foreign_key: true
+      t.references :food, foreign_key: true
 
       t.timestamps
     end
