68daa7df94078b1e4ba9c1a254b7d458d61948c5
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-12-20 13:31:51 +0530

    Add recipe route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   root 'homes#index'
+  
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
 end
