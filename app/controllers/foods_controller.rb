855cc7e51531f59b8e9c847cdd199ea309b20f63
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-12-20 22:16:47 +0500

    create index.html.erb and new.html.erb for food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,10 +6,13 @@ class FoodsController < ApplicationController
 
     def create
         @food = Food.new(food_params)
+        @food.user = current_user
+        puts "Food before save: #{@food.inspect}"
         if @food.save
          redirect_to foods_path, notice: "Your food has been added successfully"
         else
-         render :new, notice: "There is an error in adding a food"
+         puts "Errors: #{@food.errors.full_messages.inspect}"
+         render :new, alert: "There is an error in adding a food"
         end
     end
 
@@ -19,6 +22,6 @@ class FoodsController < ApplicationController
 
     private 
     def food_params
-        params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
+        params.require(:food).permit(:name, :quantity, :measurement_unit, :price)
     end
 end
